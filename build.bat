99ad66325cc26e4d2741f75b01ec81d00b340ff1
Glitched Polygons <glitchedpolygons@protonmail.com>
2020-07-19 23:07:47 +0200

    Modified build.bat

--- build.bat
+++ build.bat
@@ -4,5 +4,6 @@ if exist %out% ( rd /s /q %out% )
 mkdir %out% && cd %out%
 cmake -DBUILD_SHARED_LIBS=Off -DUSE_SHARED_MBEDTLS_LIBRARY=Off -DPWCRYPT_ENABLE_TESTS=Off -DCMAKE_BUILD_TYPE=Release ..
 msbuild pwcrypt_cli.vcxproj /p:configuration=release
+mkdir include
+xcopy ..\include .\include
 cd ..
-
