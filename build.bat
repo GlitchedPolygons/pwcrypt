255c3350522ee81befa8b2bc699dd11482e17440
Glitched Polygons <glitchedpolygons@protonmail.com>
2020-07-19 22:55:31 +0200

    Added build.bat

---
+++ build.bat
@@ -0,0 +1,8 @@
+SET repo=%~dp0
+SET out="%repo%\build-msvc"
+if exist %out% ( rd /s /q %out% )
+mkdir %out% && cd %out%
+cmake -DBUILD_SHARED_LIBS=Off -DUSE_SHARED_MBEDTLS_LIBRARY=Off -DPWCRYPT_ENABLE_TESTS=Off -DCMAKE_BUILD_TYPE=Release ..
+msbuild pwcrypt_cli.vcxproj /p:configuration=release
+cd ..
+
