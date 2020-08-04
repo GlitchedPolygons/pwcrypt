f17465101108f743852f2794ff3c91b24d4adc36
Glitched Polygons <glitchedpolygons@protonmail.com>
2020-08-04 11:18:37 +0200

    Recompiled docs/

--- docs/search/defines_0.js
+++ docs/search/defines_0.js
@@ -1,7 +1,10 @@
 var searchData=
 [
-  ['pwcrypt_5fargon2_5fm_5fcost_26',['PWCRYPT_ARGON2_M_COST',['../pwcrypt_8h.html#a1df1498104833d580084582d6d22be37',1,'pwcrypt.h']]],
-  ['pwcrypt_5fargon2_5fparallelism_27',['PWCRYPT_ARGON2_PARALLELISM',['../pwcrypt_8h.html#aecd608ec2f8cb926c6ff225d25b6a653',1,'pwcrypt.h']]],
-  ['pwcrypt_5fargon2_5ft_5fcost_28',['PWCRYPT_ARGON2_T_COST',['../pwcrypt_8h.html#a44275f6b5f0879ead495ddfce628e0e3',1,'pwcrypt.h']]],
-  ['pwcrypt_5fz_5fchunksize_29',['PWCRYPT_Z_CHUNKSIZE',['../pwcrypt_8h.html#a2f42bd2f1c4b44339f65fbde2b369826',1,'pwcrypt.h']]]
+  ['pwcrypt_5falgo_5fid_5faes256_5fgcm_29',['PWCRYPT_ALGO_ID_AES256_GCM',['../pwcrypt_8h.html#a37ca2e4b7c30369b68839f779ce3e9c5',1,'pwcrypt.h']]],
+  ['pwcrypt_5falgo_5fid_5fchacha20_5fpoly1305_30',['PWCRYPT_ALGO_ID_CHACHA20_POLY1305',['../pwcrypt_8h.html#a1b00e22c5af3cd2b4929e77a5bbda727',1,'pwcrypt.h']]],
+  ['pwcrypt_5fargon2_5fm_5fcost_31',['PWCRYPT_ARGON2_M_COST',['../pwcrypt_8h.html#a1df1498104833d580084582d6d22be37',1,'pwcrypt.h']]],
+  ['pwcrypt_5fargon2_5fparallelism_32',['PWCRYPT_ARGON2_PARALLELISM',['../pwcrypt_8h.html#aecd608ec2f8cb926c6ff225d25b6a653',1,'pwcrypt.h']]],
+  ['pwcrypt_5fargon2_5ft_5fcost_33',['PWCRYPT_ARGON2_T_COST',['../pwcrypt_8h.html#a44275f6b5f0879ead495ddfce628e0e3',1,'pwcrypt.h']]],
+  ['pwcrypt_5fversion_34',['PWCRYPT_VERSION',['../pwcrypt_8h.html#a6f984651db551e9e4b63d1e011208749',1,'pwcrypt.h']]],
+  ['pwcrypt_5fz_5fchunksize_35',['PWCRYPT_Z_CHUNKSIZE',['../pwcrypt_8h.html#a2f42bd2f1c4b44339f65fbde2b369826',1,'pwcrypt.h']]]
 ];
--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['dev_5furandom_16',['dev_urandom',['../pwcrypt_8h.html#aa689a5dedcc46f81436012f82cccd40b',1,'pwcrypt.h']]]
+  ['dev_5furandom_19',['dev_urandom',['../pwcrypt_8h.html#aa689a5dedcc46f81436012f82cccd40b',1,'pwcrypt.h']]]
 ];
