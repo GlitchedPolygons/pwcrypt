a49d0fd76b055d61012c5aee78241667f230b0c4
Glitched Polygons <glitchedpolygons@protonmail.com>
2020-08-17 22:11:38 +0200

    Modified build.bat

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
+SET i=%CD%
 SET repo=%~dp0
 SET out="%repo%\build-msvc"
 if exist %out% ( rd /s /q %out% )
 mkdir %out% && cd %out%
 cmake -DBUILD_SHARED_LIBS=Off -DUSE_SHARED_MBEDTLS_LIBRARY=Off -DPWCRYPT_ENABLE_TESTS=Off -DCMAKE_BUILD_TYPE=Release ..
 msbuild pwcrypt_cli.vcxproj /p:configuration=release
-cd Release
-mkdir include
-xcopy ..\..\include .\include
-tar -czvf ..\pwcrypt.tar.gz *
-cd ..\..
+mkdir include\pwcrypt
+xcopy ..\include\pwcrypt .\include\pwcrypt
+tar -czvf pwcrypt.tar.gz include\pwcrypt\* Release\pwcrypt.lib Release\pwcrypt_cli.exe
+cd %i%
